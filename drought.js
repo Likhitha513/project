384fcd43b45b7b60ac2321d39108fb5de385b173
Balaramaraju07 <rajuaddada1331@gmail.com>
2025-04-23 00:22:36 +0530

    Update drought.js

--- drought.js
+++ drought.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tips = [
     "Before: Prepare for Potential Risks.",
-    "During: monitor weather patterns.",
+    "During: Monitor weather patterns.",
     "During: Monitor water usage and water levels in wells.",
     "After: Be cautious of water contamination and electric shocks.",
     "After: Assess the impact of drought on ecosystems."
   ];
 
   const tipsList = document.getElementById("tipsList");
-  tips.forEach(tip => {
+  tips.forEach((tip, index) => {
     const li = document.createElement("li");
     li.textContent = tip;
+    li.style.opacity = "0";
+    li.style.animation = `fadeIn 0.3s ease forwards ${index * 0.1}s`;
     tipsList.appendChild(li);
   });
 });
