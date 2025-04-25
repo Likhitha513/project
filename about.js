675cd72fbe74868b9f2715db8388a891330331af
sailakshmisri84 <sailakshmisri.d@gmail.com>
2025-04-25 22:10:03 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,8 +1,16 @@
-// You can use this to dynamically add team members in future
-// For now, we’ll leave it as a placeholder
+
 document.getElementById("teamList").innerHTML = `
-  <li>Likhitha– Project Lead</li>
+  <li>Likhitha – Project Lead</li>
   <li>Sai Lakshmi – Emergency Analyst</li>
   <li>Rama Raju – Field Coordinator</li>
-  <li>Arief– Technical Support</li>
+  <li>Arief – Technical Support</li>
 `;
+
+// Navigation logic (update URLs if needed)
+function goBack() {
+  window.location.href = "index.html"; // or set previous section/page
+}
+
+function goNext() {
+  window.location.href = "alerts.html"; // or set next section/page
+}
